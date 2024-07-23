154a7f796279c4bd8fb8adbd3c2d49cb129c40e7
Sarah Ocy <95500466+Sarocy@users.noreply.github.com>
2024-07-23 11:19:45 -0300

    Add files via upload

---
+++ day02_selects.sql
@@ -0,0 +1,15 @@
+SELECT * FROM filmes;
+
+SELECT * FROM diretores;
+
+SELECT * FROM generos;
+
+SELECT * FROM avaliacoes;
+
+SELECT * FROM diretores WHERE nome LIKE 'A%';
+
+SELECT * FROM filmes WHERE titulo LIKE '%Os%';
+
+SELECT * FROM generos WHERE nome LIKE '%e';
+
+SELECT * FROM avaliacoes WHERE valor > 4;
